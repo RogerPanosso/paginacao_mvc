a7072cf352b59345c50cd2d9796bca72382a1237
Roger Panosso <rogerninopa@gmail.com>
2021-10-27 20:23:30 -0300

    Criando perante camada Core(auxiliar) do projeto arquivo Controller.php

---
+++ App/Core/Controller.php
@@ -0,0 +1,31 @@
+<?php
+	/*
+	* Definição da class Controller(auxiliar) sendo uma class base para ser herdada diante seus 
+	* controllers de acessos perante aplicação, realizando as devidas requisições ao usuário final
+	*/
+	namespace App\Core;
+
+	class Controller {
+		protected array $dados;
+
+		public function __construct() {
+			$this->dados = array();
+		}
+
+		public function loadTemplate(string $nomeView, array $dados = array()) {
+			$this->dados = $dados;
+			require_once __DIR__."/../App/Config/Config.php";
+			require __DIR__."/../App/Views/Parcial/template.php";
+		}
+
+		public function loadViewInTemplate(string $nomeView, array $dados = array()) {
+			extract($this->dados);
+			require __DIR__."/../App/Views/Pages/".$nomeView.".php";
+		}
+
+		public function loadView(string $nomeView, array $dados = array()) {
+			extract($dados);
+			require __DIR__."/../App/Views/Pages/".$nomeView.".php";
+		}
+	}
+?>
