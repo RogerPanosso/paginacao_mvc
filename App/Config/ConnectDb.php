c0fd4037b94747c922cb5c7d7615716e8f1a52a9
Roger Panosso <rogerninopa@gmail.com>
2021-10-27 19:33:11 -0300

    Criando perante camada Publica(Public) do projeto arquivos index.php e .htaccess

---
+++ App/Config/ConnectDb.php
@@ -0,0 +1,8 @@
+<?php
+	/*
+	* Definição da class ConnectDb sendo responsavel por estabelecer conexão com servidor de banco
+	* de dados através da biblioteca PDO(object). Utilizando padrão de projeto criacional Singleton
+	* contendo instância única de objeto
+	*/
+	
+?>
