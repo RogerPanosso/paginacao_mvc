e4b5533e19d6b909cbfe824bbd893cfa443c0e7f
Roger Panosso <rogerninopa@gmail.com>
2021-10-27 19:47:31 -0300

    Criando perante camada de Config(configurações) do projeto arquivo ConnectDb.php contendo class na qual realiza conexão com servidor de banco de dados

--- App/Config/ConnectDb.php
+++ App/Config/ConnectDb.php
@@ -4,5 +4,25 @@
 	* de dados através da biblioteca PDO(object). Utilizando padrão de projeto criacional Singleton
 	* contendo instância única de objeto
 	*/
-	
+	namespace App\Config;
+
+	class ConnectDb {
+		private static ?\PDO $instancePdo = null;
+
+		private function __construct(){}
+		private function __destruct(){}
+		private function __clone(){}
+		private function __wakeup(){}
+
+		public static function getInstancePdo() : \PDO {
+			if(!isset(self::$instancePdo) and self::$instancePdo === null):
+				require_once __DIR__."/Config.php";
+				self::$instancePdo = new \PDO($config["dbdriver"].":dbname=".$config["dbname"].";host=".$config["dbhost"], $config["dbuser"], $config["dbpass"]);
+				self::$instancePdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+				self::$instancePdo->setAttribute(\PDO::ATTR_ORACLE_NULLS, \PDO::NULL_EMPTY_STRING);
+			endif;
+			//caso já exista uma instancia PDO(object) criada e armazenada em atributo estático somente retorna
+			return self::$instancePdo;
+		}
+	}
 ?>
